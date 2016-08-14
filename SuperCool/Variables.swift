9797369db489069816b3aace5bb37a9308bff976
Fatih Durdu <mfdurdu@gmail.com>
2016-08-14 23:14:56 +0300

    Added initial project files

---
+++ SuperCool/Variables.swift
@@ -0,0 +1,20 @@
+//
+//  Variables.swift
+//  SuperCool
+//
+//  Created by Fatih Durdu on 12/08/16.
+//  Copyright © 2016 Fatih Durdu. All rights reserved.
+//
+
+import UIKit
+
+var welcomeMessage = "Hello ,Your account is overdownd. Dont you know how to manage money dummy!"
+
+var accountBalance = -500.0
+
+var dicimal = 50.5
+
+let newDecimal = 40.0
+
+var acoount1 = 0.0,acooun2 = 12
+
